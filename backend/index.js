178f8f82de2fa759831f1f23bf6e3f1d24628e90
nihal kumar <nihalkbarnwal20@gmail.com>
2024-09-02 18:01:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'https://wchat-wxmp.vercel.app/',
+    origin:'https://wchat-wxmp.vercel.app',
     credentials:true
 };
 app.use(cors(corsOption)); 
